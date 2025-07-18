576fb66d02ed3ce5362f1e4d8007b8ec91e50c93
yogeshdeshmukh940 <deshmukhyogesh746@gmail.com>
2025-07-18 12:31:35 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-// import toast from "react-hot-toast";
 import { toast } from "react-toastify";
 import { useNavigate } from 'react-router-dom';
-
+//awe//
 function Login() {
 
   const [formData , setFormData]=useState({})
